0db89963ca4bc132ad2ba345db529ef4113072b9
Amir Lotfi <lotfi1378.al@gmail.com>
2024-01-03 20:58:46 +0330

    fourth modification. added more exception handlers

--- src/main/java/us/core/pr/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/us/core/pr/exception/handler/GlobalExceptionHandler.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import us.core.pr.exception.IllegalHeadOfDepartmentAssignmentException;
 import us.core.pr.exception.RecordNotFoundException;
+import us.core.pr.exception.entity.CollegeRecordNotFoundException;
+import us.core.pr.exception.entity.CourseRecordNotFoundException;
+import us.core.pr.exception.entity.ProfessorRecordNotFoundException;
+import us.core.pr.exception.entity.StudentRecordNotFoundException;
 
 import javax.persistence.EntityNotFoundException;
 import java.io.Serializable;
@@ -17,22 +21,48 @@ public class GlobalExceptionHandler
     public GlobalExceptionHandler() {}
 
     @ExceptionHandler(value = IllegalHeadOfDepartmentAssignmentException.class)
-    public ResponseEntity<Object> IllegalHeadOfDepartmentAssignmentExceptionHandler(Exception exception, WebRequest webRequest)
+    public ResponseEntity<Object> illegalHeadOfDepartmentAssignmentExceptionHandler(Exception e, WebRequest webRequest)
     {
         return null;
     }
 
     @ExceptionHandler(value = RecordNotFoundException.class)
-    public ResponseEntity<Object> RecordNotFoundExceptionHandler(Exception exception, WebRequest webRequest)
+    public ResponseEntity<Object> recordNotFoundExceptionHandler(Exception e, WebRequest webRequest)
     {
         return null;
     }
 
+
     @ExceptionHandler(value = EntityNotFoundException.class)
-    public ResponseEntity<Object> EntityNotfoundExceptionHandler(Exception exception, WebRequest webRequest)
+    public ResponseEntity<Object> entityNotfoundExceptionHandler(Exception e, WebRequest webRequest)
     {
         return null;
     }
+
+    @ExceptionHandler(value = CollegeRecordNotFoundException.class)
+    public ResponseEntity<Object> collegeRecordNotFoundExceptionHandler(Exception e, WebRequest webRequest)
+    {
+        return null;
+    }
+
+    @ExceptionHandler(value = CourseRecordNotFoundException.class)
+    public ResponseEntity<Object> courseRecordNotFoundExceptionHandler(Exception e, WebRequest webRequest)
+    {
+        return null;
+    }
+
+    @ExceptionHandler(value = ProfessorRecordNotFoundException.class)
+    public ResponseEntity<Object> professorRecordNotFoundExceptionHandler(Exception e, WebRequest webRequest)
+    {
+        return null;
+    }
+
+    @ExceptionHandler(value = StudentRecordNotFoundException.class)
+    public ResponseEntity<Object> studentRecordNotFoundExceptionHandler(Exception e, WebRequest webRequest)
+    {
+        return null;
+    }
+
 
 }
 
